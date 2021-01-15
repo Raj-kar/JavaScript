c378baf02518a2a6b931cafe1ea4626781c8012c
FlutterRaj <rajkar921@gmail.com>
2021-01-15 13:01:16 +0530

    initial added

---
+++ From Basic/main.js
@@ -0,0 +1,27 @@
+<input type="text" id="num1">
+    <input type="text" id="op">
+        <input type="text" id="num2">
+            <button onclick="calculate()">Click me</button>
+            <script>
+                function calculate() {
+                    console.log("CLICK! ");
+            document.write(":)");
+        }
+
+
+    // switch (operator) {
+        //     case "+": document.write(num1 + num2);
+        //         break;
+
+        //     case "-": document.write(num1 - num2);
+        //         break;
+
+        //     case "*": document.write(num1 * num2);
+        //         break;
+
+        //     case "/": document.write(num1 / num2);
+        //         break;
+
+        //     default: 
+        //         break;
+        // }
