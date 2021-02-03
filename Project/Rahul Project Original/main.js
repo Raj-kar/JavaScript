708478abeda11e5298491ab2135ec90795bbbfdf
FlutterRaj <rajkar921@gmail.com>
2021-02-04 03:08:42 +0530

    add comments on Rahul Project

--- Project/Rahul Project Original/main.js
+++ Project/Rahul Project Original/main.js
@@ -1,16 +1,20 @@
+// This function will invoke when user fills all required fields and press Calculate button.
 function calculate_bill() {
+    // Fetch all user inputs using "getElementById" method.
     var category = document.getElementById("arc").value;
     var meter_number = Number(document.getElementById("meter").value);
     var units = Number(document.getElementById("units").value);
-    var total_price = 0;
+    var total_price = 0; // a variable for addup the total cost.
 
     if (category === "Agriculture") {
+        // If user choose category Agriculture
         if (units <= 1000) {
             total_price += units * 5;
         } else {
             total_price += ((1000 * 5) + (units - 1000) * 6);
         }
     } else if (category === "Residential") {
+        // If user choose category Residential
         if (units <= 100) {
             total_price += units * 5;
         } else if (units <= 200) {
@@ -23,12 +27,13 @@ function calculate_bill() {
             total_price += ((100 * 5) + (100 * 5.5) + (100 * 6) + (100 * 6.5) + (units - 400) * 7);
         }
     } else {
+        // If user choose category Commercial
         if (units <= 1000) {
             total_price += units * 8;
         } else {
             total_price += ((1000 * 8) + (units - 1000) * 10) + 1000;
         }
     }
-
+    // Hold the p tag with "getElementById", and enter data using "innerHTML" method.
     document.getElementById("output").innerHTML = `User Meter number = ${meter_number} <br> Total bill = ${total_price}`;
 }
